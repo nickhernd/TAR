48dfc9577e11d0be4879689fd13333136d6771a2
nickhernd <ramajahede@gmail.com>
2026-03-22 21:05:43 +0100

    jaime: finish mem and record vid

---
+++ practica_2_2526/build/turtlebot3_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/workspace/install/turtlebot3_description/share/turtlebot3_description/package.sh"
+. "/workspace/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
+. "/workspace/install/turtlebot3_node/share/turtlebot3_node/package.sh"
---
+++ practica_2_2526/build/turtlebot3_simulations/colcon_command_prefix_build.sh
@@ -0,0 +1,5 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/workspace/install/turtlebot3_gazebo/share/turtlebot3_gazebo/package.sh"
+. "/workspace/install/turtlebot3_manipulation_gazebo/share/turtlebot3_manipulation_gazebo/package.sh"
+. "/workspace/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
+. "/workspace/install/turtlebot3_fake_node/share/turtlebot3_fake_node/package.sh"
